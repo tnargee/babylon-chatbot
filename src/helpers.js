fbee6306e911d06e89b137b2378f9efd67373ed4
Arthur Wu <arthur.kainan.wu@gmail.com>
2024-01-08 15:14:47 -0500

    fixed bugs relating to API calls

--- src/helpers.js
+++ src/helpers.js
@@ -1,28 +1,35 @@
 import { OpenAI } from "openai";
 
-export async function query(question, client, thread) {
-    // const client = new OpenAI(
-    //     {   
-    //         //change this back to the actual API key once testing is finished
-    //         // DO NOT LEAVE AS COMMENT
-    //         // apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-    //         apiKey: "myKey", //change back when testing
-    //         dangerouslyAllowBrowser: true,
-    //     }
-    // );
 
-    // let thread = await client.beta.threads.create();
+const client = new OpenAI(
+    {   
+        //change this back to the actual API key once testing is finished
+        // DO NOT LEAVE AS COMMENT
+        // apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+        apiKey: "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", //change back when testing
+        dangerouslyAllowBrowser: true,
+    }
+);
+
+export async function createThread() {
+    let thread = await client.beta.threads.create();
+    return thread.id;
+}
+
+export async function query(question, threadID) {
 
     let message = await client.beta.threads.messages.create(
-        thread.id, 
+        threadID,
         {
             role: "user",
             content: question,
         }
     )
 
+    
+
     let run = await client.beta.threads.runs.create(
-        thread.id,
+        threadID,
         {
             assistant_id: "asst_8hyA2QINdC17K82EveO6Pcn3" //change to real key when testing
             // assistantID: import.meta.env.ASSISTANT_ID,
@@ -32,13 +39,13 @@ export async function query(question, client, thread) {
 
     while (true) {
         let runStatus = await client.beta.threads.runs.retrieve(
-            thread.id,
+            threadID,
             run.id
         );
 
         if (runStatus.status == "completed"){
-            const msgs = await client.beta.threads.messages.list(thread.id)
-            console.log(msgs.data.map((datapiece) => {console.log(datapiece.content[0].text.value)}));
+            console.log("Run finished")
+            const msgs = await client.beta.threads.messages.list(threadID)
             return msgs.data[0].content[0].text.value;
             break;
         } else {
@@ -47,17 +54,3 @@ export async function query(question, client, thread) {
     }
 
 };
-
- const client = new OpenAI(
-        {   
-            //change this back to the actual API key once testing is finished
-            // DO NOT LEAVE AS COMMENT
-            // apiKey: import.meta.env.VITE_OPENAI_API_KEY,
-            apiKey: "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", //change back when testing
-            dangerouslyAllowBrowser: true,
-        }
-    );
-
-let thread = await client.beta.threads.create();
-
-console.log(await query("Help me with this Python code: print('hello'); it's not compiling", client, thread));
